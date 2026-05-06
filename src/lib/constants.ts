dabb33b9f5364ee4667d166caffa33e871a37b90
nikissXI <1299577815@qq.com>
2026-05-06 17:42:00 +0800

    动画修复和冗余代码去除

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,6 @@
 export const SITE_DESCRIPTION = "nothing for now";
 export const PAGE_SIZE = 5;
 
-export const CATEGORIES: Record<string, string> = {
-  "1": "技术杂谈",
-  "2": "WEB渗透",
-  "3": "路由交换",
-  "4": "Linux运维",
-};
-
 export const FRIEND_LINKS = [
   { name: "github", url: "http://github.com/nikissXI" },
   { name: "B站", url: "https://space.bilibili.com/18052798" },
